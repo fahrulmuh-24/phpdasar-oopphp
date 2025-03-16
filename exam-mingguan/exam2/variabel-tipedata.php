7489b3d6c78e6272660452304cdc2bf99b9f545f
fahrulmuh-24 <muhamfahrul2006@gmail.com>
2025-03-17 00:01:46 +0700

    exam 2

---
+++ exam-mingguan/exam2/variabel-tipedata.php
@@ -0,0 +1,48 @@
+<?php
+/*
+1. Buat sebuah variabel yang menyimpan nilai string dan tampilkan hasilnya.
+2. Buat sebuah variabel yang menyimpan nilai integer, kemudian ubah nilai variabel tersebut 
+   dengan menambahkan 5.
+3. Buat sebuah variabel boolean dengan nilai "true" dan tampilkan hasilnya.
+4. Buat sebuah array yang berisi angka dari 1 sampai 5, kemudian tampilkan elemen pertama 
+   dan terakhir dari array tersebut.  
+5. Tentukan tipe data untuk variabel berikut dan tampilkan tipe datanya:
+   <?php
+   $satu = ‘satu’;
+   $dua = 432809;
+   $tiga = true;
+   $empat = [false,’true’];
+6. Buat sebuah variabel yang menyimpan float dan ubah nilai variabel tersebut menjadi hasil 
+   perkalian dengan 2.
+   $pertama = 0.5;
+   $kedua = 392.42;
+*/
+
+//1.
+// $nama = "Fahrul";
+// echo $nama;
+
+// 2.
+// $angka = 10;
+// $angka = $angka + 5;
+// echo $angka;
+
+// 3.
+// $nilai = true;
+// echo $nilai;
+
+// 4.
+// $angka = [1, 2, 3, 4, 5];
+// echo $angka[0]; 
+// echo $angka[count($angka) - 1]; 
+
+// 5.
+// $satu = 'satu'; 
+// $dua = 432809; 
+// $tiga = true; 
+// $empat = [false, 'true']; 
+// echo gettype($satu); 
+// echo gettype($dua); 
+
+
+?>
