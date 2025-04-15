76d18b727a3de93cd1421327e03f027c6f3d07cd
fahrulmuh-24 <muhamfahrul2006@gmail.com>
2025-04-15 14:31:58 +0700

    Menambahkan exam 3

---
+++ project web/exam-php/index.php
@@ -0,0 +1,9 @@
+<?php
+
+// tanggal dan hari
+// echo date("l, d-M-y")
+
+//mengetahui tanggal lahi
+// (mktime) : jam, menit, detik, bulan, tanggal, tahun
+// echo date("l", mktime(0,0,0,2,24,2006));
+?>
